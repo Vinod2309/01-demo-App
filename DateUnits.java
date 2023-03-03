be62386d772c2380cb923bc2e9211d01d2ee0050
Vinod2309 <vinodmedicoz@hotmail.com>
2023-03-03 07:20:10 +0530

    HIS-300: Changes

--- DateUnits.java
+++ DateUnits.java
@@ -1,8 +1,9 @@
 public class DateUtils{
-
+          private static final string REST_URL="https://www.equifax.com/getScores";
 
   public String convert DateToStr(){
-   
+     
+            
     
       return "";
 
