713ae7bd159acb615b25f74579ff72473e266ad5
Vinod2309 <vinodmedicoz@hotmail.com>
2023-03-03 07:31:46 +0530

    HIS-301: changes

--- DateUnits.java
+++ DateUnits.java
@@ -1,12 +1,17 @@
 public class DateUtils{
-
-
+            private Map<String,Object> cache = new HASHMap<String,Object>()
+        
   public String convert DateToStr(){
+
    
     
       return "";
 
   }
+            //HIS-301 changes
+       public void loadDataToCache(){
+            //logic
 
+       }
 
 }
